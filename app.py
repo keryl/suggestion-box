efae42ac5c17e58ff8fe773512225bb3602601c0
keryl <kerylklonye@gmail.com>
2017-03-14 22:31:41 +0300

    Add login, signup and logout routes

--- app.py
+++ app.py
@@ -1,12 +1,31 @@
 from flask import Flask, request, session, render_template, redirect, flash
 
-# create app
-
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Home"
+    return render_template('home_logged_out.html', title='Home')
+
+# signup route
+
+@app.route("/signup", methods=['GET', 'POST'])
+def signup():
+    # if GET, display signup page
+    return render_template('signup.html', title='Sign Up')
+
+# login route
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    return render_template('login.html', title='Login')
+
+# logout route
+
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    return redirect("/")
+
+# start app
 
 if __name__ == "__main__":
     app.run(port=3000, debug=True)
