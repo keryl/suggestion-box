44205ee205a2ed109c7d7fe4cd17f72c9ce195d3
keryl <kerylklonye@gmail.com>
2017-03-14 22:38:32 +0300

    Implement session management

--- app.py
+++ app.py
@@ -4,25 +4,43 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('home_logged_out.html', title='Home')
+    is_logged_in = 'user_id' in session
+    if is_logged_in:
+        return render_template('home_logged_in.html', title='Home')
+    else:
+        return render_template('home_logged_out.html', title='Home')
 
 # signup route
 
 @app.route("/signup", methods=['GET', 'POST'])
 def signup():
+    error = None
+
+    if request.method == 'POST':
+        # if POST, add user to database
+        pass
     # if GET, display signup page
-    return render_template('signup.html', title='Sign Up')
+    return render_template('signup.html', title='Sign Up', error=error)
 
 # login route
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    return render_template('login.html', title='Login')
+    error = None
+
+    if request.method == 'POST':
+        # if POST, check if an existing user exists in the DB
+        # if so, log them in i.e.
+        pass
+    # if GET, display login page
+    return render_template('login.html', title='Login', error=error)
 
 # logout route
 
 @app.route("/logout", methods=['GET', 'POST'])
 def logout():
+    if 'user_id' in session:
+        del session["user_id"]
     return redirect("/")
 
 # start app
